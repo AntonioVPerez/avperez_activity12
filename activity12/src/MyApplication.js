078c9656a2233e1e9efb1c13054359ad26f701ee
AntonioVPerez <122927677+AntonioVPerez@users.noreply.github.com>
2024-04-05 21:32:35 +0000

    2nd

--- activity12/src/MyApplication.js
+++ activity12/src/MyApplication.js
@@ -1,37 +1,54 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import "bootstrap/dist/css/bootstrap.css";
 
 const { register, handleSubmit, formState: { errors } } = useForm();
 const [dataF,setDataF] = useState({});
 const [viewer,setViewer] = useState(0);
 
 function Payment(){
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit(onSubmit)} className="container mt-5"> 
 
-    <input {...register("fullName", { required: true })} placeholder="Full Name" />
-    {errors.fullName && <p>Full Name is required.</p>}
+    <div className="form-group">
+        <input {...register("fullName", { required: true })} placeholder="Full Name" className="form-control" />
+        {errors.fullName && <p className="text-danger">Full Name is required.</p>}
+    </div>
 
-    <input {...register("email", { required: true, pattern: /^\S+@\S+$/i })} placeholder="Email" />
-    {errors.email && <p>Email is required.</p>}
+    <div className="form-group">
+        <input {...register("email", { required: true, pattern: /^\S+@\S+$/i })} placeholder="Email" className="form-control" />
+        {errors.email && <p className="text-danger">Email is required.</p>}
+    </div>
 
-    <input {...register("creditCard", { required: true })} placeholder="Credit Card" />
-    {errors.creditCard && <p>Credit Card is required.</p>}
+    <div className="form-group">
+        <input {...register("creditCard", { required: true })} placeholder="Credit Card" className="form-control" />
+        {errors.creditCard && <p className="text-danger">Credit Card is required.</p>}
+    </div>
     
-    <input {...register("address", { required: true })} placeholder="Address" />
-    {errors.address && <p>Address is required.</p>}
+    <div className="form-group">
+        <input {...register("address", { required: true })} placeholder="Address" className="form-control" />
+        {errors.address && <p className="text-danger">Address is required.</p>}
+    </div>
     
-    <input {...register("address2")} placeholder="Address 2" />
+    <div className="form-group">
+        <input {...register("address2")} placeholder="Address 2" className="form-control"/>
+    </div>
     
-    <input {...register("city", { required: true })} placeholder="City" />
-    {errors.city && <p>City is required.</p>}
+    <div className="form-group">
+        <input {...register("city", { required: true })} placeholder="City" className="form-control" />
+        {errors.city && <p className="text-danger">City is required.</p>}
+    </div>
     
-    <input {...register("state", { required: true })} placeholder="State" />
-    {errors.state && <p>State is required.</p>}
+    <div className="form-group">
+        <input {...register("state", { required: true })} placeholder="State" className="form-control" />
+        {errors.state && <p className="text-danger">State is required.</p>}
+    </div>
     
-    <input {...register("zip", { required: true })} placeholder="Zip" />
-    {errors.zip && <p>Zip is required.</p>}
+    <div className="form-group">
+        <input {...register("zip", { required: true })} placeholder="Zip" className="form-control" />
+        {errors.zip && <p className="text-danger">Zip is required.</p>}
+    </div>
 
-    <button type="submit">Submit</button>
+    <button type="submit" className="btn btn-primary">Submit</button>
 
     </form>
 
@@ -55,12 +72,13 @@ function App(){
             setDataF(dataF);
         };
 
-        <button onClick={updateHooks}>Submit</button>
+        <button onClick={updateHooks} className="btn btn-secondary">Submit</button>
 
         return (<div>
             <h1>Payment summary:</h1>
             <h3>{dataF.fullName}</h3>
             <p>{dataF.email}</p>
+            <p>{dataF.creditCard}</p>
             ...
             <p>{dataF.city},{dataF.state} {dataF.zip} </p>
         </div>);
